7fb4b6c686de5a4c30307179673c00f46e752d9c
JorgeSL20 <20201175@uthh.edu.mx>
2024-12-12 14:34:53 -0600

    despliegue

--- App.js
+++ App.js
@@ -3,6 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/Ionicons'; // Importa los íconos
+import { Text } from 'react-native';
+
+
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -12,9 +15,12 @@ import UserScreen from './screens/UserScreen';
 import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
-  dsn: 'https://74b71162cd8239d7efd7e673f26301f2@o4508439367778304.ingest.us.sentry.io/4508439489347584',
-  // Otras configuraciones si las necesitas
+  dsn: 'https://408d81b9692f59b527576740c02aae00@o4508439367778304.ingest.us.sentry.io/4508439796318208',
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // enableSpotlight: __DEV__,
 });
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -63,20 +69,29 @@ const HomeTabs = () => (
   </Tab.Navigator>
 );
 
-export default function App() {
+function App() {
   // Error intencional para probar Sentry
   useEffect(() => {
-    // Lanza un error para probar la integración de Sentry
-    throw new Error('My first Sentry error!');
+    try {
+      // Lanza un error para probar la integración de Sentry
+      throw new Error('My first Sentry error!');
+    } catch (error) {
+      Sentry.captureException(error); // Captura el error y lo envía a Sentry
+    }
   }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Iniciar Sesión' }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Registrarse' }} />
-        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Iniciar Sesión' }} />
+          <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'Registrarse' }} />
+          <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    
   );
 }
+
+
+export default Sentry.wrap(App);
