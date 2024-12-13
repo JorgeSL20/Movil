ce989671ae986b1e93893914a58775f43f0192b6
JorgeSL20 <20201175@uthh.edu.mx>
2024-12-13 06:22:59 -0600

    despliegue bueno

--- App.js
+++ App.js
@@ -69,10 +69,7 @@ const HomeTabs = () => (
 
 function App() {
   // Función para generar un error intencionalmente
-  const generateError = () => {
-    throw new Error("Este es un error de prueba para Sentry");
-  };
-
+ 
 
 
   return (
